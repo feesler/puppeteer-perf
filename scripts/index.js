c93b219b62268534970e24105c7d80c72205f795
Ilya Makarov <feesler@gmail.com>
2024-02-20 22:01:40 +0300

    * Moved puppeteer performance test task to separate file

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,12 @@
-import puppeteer from 'puppeteer';
-import { performance } from 'perf_hooks';
-import { version } from 'process';
-import { readFileSync } from 'fs';
+import { task } from './task.js';
 
-const getPackageVersion = (name) => {
-    const fileName = `node_modules/${name}/package.json`;
-    const content = readFileSync(fileName);
-    const json = JSON.parse(content);
-    return json.version;
+const run = async () => {
+    const res = await task();
+
+    console.log(`Node: ${res.node}`);
+    console.log(`Puppeteer: ${res.puppeteer}`);
+    console.log(`Browser: ${res.browser}`);
+    console.log(`Duration: ${res.duration}`);
 };
 
-(async () => {
-    const URL = 'https://google.com';
-    const pptrVersion = getPackageVersion('puppeteer');
-
-    const launchOptions = {
-        headless: 'new',
-    };
-
-    const browser = await puppeteer.launch(launchOptions);
-    const browserVersion = await browser.version();
-    const allPages = await browser.pages();
-    const page = (allPages.length) ? allPages[0] : await browser.newPage();
-    await page.goto(URL, { waitUntil: 'load' });
-
-    /** Start measure after page loaded to discard network delays */
-    const startTime = performance.now();
-
-    const elements = await page.$$('*');
-    const tasks = elements.map((elem) => elem.evaluate((el) => el.className));
-    const data = await Promise.all(tasks);
-    console.log(data.join(' '));
-
-    console.log(`Node: ${version}`);
-    console.log(`Puppeteer: ${pptrVersion}`);
-    console.log(`Browser: ${browserVersion}`);
-    console.log(`Duration: ${performance.now() - startTime}`);
-
-    await browser.close();
-})();
+run();
---
+++ scripts/task.js
@@ -0,0 +1,44 @@
+import puppeteer from 'puppeteer';
+import { performance } from 'perf_hooks';
+import { version } from 'process';
+import { readFileSync } from 'fs';
+
+const getPackageVersion = (name) => {
+    const fileName = `node_modules/${name}/package.json`;
+    const content = readFileSync(fileName);
+    const json = JSON.parse(content);
+    return json.version;
+};
+
+export const task = async () => {
+    const URL = 'https://google.com';
+    const pptrVersion = getPackageVersion('puppeteer');
+
+    const launchOptions = {
+        headless: 'new',
+    };
+
+    const browser = await puppeteer.launch(launchOptions);
+    const browserVersion = await browser.version();
+    const allPages = await browser.pages();
+    const page = (allPages.length) ? allPages[0] : await browser.newPage();
+    await page.goto(URL, { waitUntil: 'load' });
+
+    /** Start measure after page loaded to discard network delays */
+    const startTime = performance.now();
+
+    const elements = await page.$$('*');
+    const tasks = elements.map((elem) => elem.evaluate((el) => el.className));
+    await Promise.all(tasks);
+
+    const res = {
+        node: version,
+        puppeteer: pptrVersion,
+        browser: browserVersion,
+        duration: performance.now() - startTime,
+    };
+
+    await browser.close();
+
+    return res;
+}
